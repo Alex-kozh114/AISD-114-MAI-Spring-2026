58ff980bb8cf40fca10da8768e5729f702b3dc1f
Alex-kozh114 <mafioznca@gmail.com>
2026-03-24 00:28:00 +0300

    Update solution.sh

--- lab1/solution.sh
+++ lab1/solution.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 show_help() {
-    echo "Usage: $0 [OPTION]... DIRECTORY DEL_SUFFIX CTRL_SUFFIX"
+    echo "Usage: $0 [DIRECTORY] [DEL_SUFFIX] [CTRL_SUFFIX]"
     echo "Delete files with DEL_SUFFIX if a file with the same name and CTRL_SUFFIX exists."
     echo ""
-    echo "Options:"
-    echo "  -h, --help          display this help and exit"
+    echo "Mandatory arguments to long options are mandatory for short options too."
+    echo "  -h, --help     display this help and exit"
 }
-if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+
+if [[ "$1" == "-h" || "$1" == "--help" || $# -lt 3 ]]; then
     show_help
     exit 0
 fi
-if [ $# -lt 3 ]; then
-    show_help
+DIR="$1"
+DEL_SUF="$2"
+CTRL_SUF="$3"
+# Проверка существования директории
+if [[ ! -d "$DIR" ]]; then
     exit 1
 fi
-target_dir="$1"
-del_suffix="$2"
-ctrl_suffix="$3"
-# Проверка: существует ли такая директория
-if [ ! -d "$target_dir" ]; then
-    exit 1
-fi
-cd "$target_dir" || exit 1
-for file in *"$del_suffix"; do
-    if [[ -e "$file" ]]; then
-        base_name="${file%"$del_suffix"}"
-        control_file="${base_name}${ctrl_suffix}"
-        if [[ -f "$control_file" ]]; then
-            rm "$file"
-        fi
+cd "$DIR" || exit 1
+for file in *"$DEL_SUF"; do
+    [[ -e "$file" ]] || continue
+    base="${file%"$DEL_SUF"}"
+    ctrl_file="${base}${CTRL_SUF}"
+    if [[ -f "$ctrl_file" ]]; then
+        rm "$file"
     fi
 done
+exit 
